9a046100bf3ada414f0942e2b91c4d18e763cdbe
jidealakija <francisalakija@gmail.com>
2023-08-24 23:26:40 +0100

    new commit

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -17,7 +17,7 @@ export default function Navbar() {
         </Link>
         <button
           onClick={toggleMenu}
-          className='md:hidden text-xl'
+          className='md:hidden text-xl hover:bg-slate-300'
         >
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
